843e60d60011cb808565e9a9789abc1846cb09de
TusharAswal <36911610+TusharAswal@users.noreply.github.com>
2018-03-17 22:39:51 +0530

    /

--- src/component/Frontpage.js
+++ src/component/Frontpage.js
@@ -6,7 +6,20 @@ import { View,Text,Button, TouchableHighlight,TouchableOpacity} from 'react-nati
 import DrawerLayoutAndroid from 'react-native-drawer-layout';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { width, height, totalSize } from 'react-native-dimension';
+import TabBar from "react-native-underline-tabbar";
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+
 import SideBarMenu from './SideBarMenu';
+import NOWPLAYING from './TABS/NOWPLAYING';
+import TOPBOXOFFICE from './TABS/TOPBOXOFFICE';
+import ANTICIPATED from './TABS/ANTICIPATED';
+import IMDBTOP250 from './TABS/IMDBTOP250';
+import NEWDVDS from './TABS/NEWDVDS';
+import TOPRATED from './TABS/TOPRATED';
+import TRENDING from './TABS/TRENDING';
+import UPCOMING from './TABS/UPCOMING';
+import UPCOMINGDVDS from './TABS/UPCOMINGDVDS';
+import ONNETFLIX from './TABS/ONNETFLIX';
 
 export default class LoginPage extends Component {
     constructor() {
@@ -26,7 +39,7 @@ export default class LoginPage extends Component {
               drawerBackgroundColor="#fff"
               drawerPosition={DrawerLayoutAndroid.positions.Left}
               renderNavigationView={() => <SideBarMenu />}>               
-              <View style={{flex: 1}}>     
+              <View style={{flex: 1,backgroundColor:'#778899'}}>     
                 <View style={{flex:0.1,flexDirection:'row' ,backgroundColor:'#778899'}}>
                 <View style={{flex:0.2,backgroundColor:'#778899',margin:10,justifyContent:'center'}}>
                     <TouchableOpacity onPress={()=>this.openDrawer()}>
@@ -52,8 +65,23 @@ export default class LoginPage extends Component {
                     </View>
                 </View>
             </View>
-            <View style={{flex:0.9,borderWidth:2}}>
-            </View>
+                <ScrollableTabView
+                tabBarActiveTextColor="white"
+                renderTabBar={() => <TabBar underlineColor="green"  
+                tabBarStyle={{ backgroundColor: "#778899"}}
+                tabBarTextStyle={{fontSize:15}}
+                />} style={{flex:0.9}}>
+                <NOWPLAYING tabLabel={{label: "NOW PLAYING"}} label="Page #1"/>
+                <TOPBOXOFFICE tabLabel={{label: "TOP BOX OFFICE"}} label="Page #2"/>
+                <ANTICIPATED tabLabel={{label: "ANTICIPATED"}} label="Page #3"/>
+                <IMDBTOP250 tabLabel={{label: "IMDB TOP 250"}} label="Page #4"/>
+                <NEWDVDS tabLabel={{label: "NEW DVDS"}} label="Page #5"/>
+                <ONNETFLIX tabLabel={{label: "ON NET FLIX"}} label="Page #6"/>
+                <TRENDING tabLabel={{label: "TRENDING"}} label="Page #7"/>
+                <UPCOMING tabLabel={{label: "UPCOMING"}} label="Page #8"/>
+                <UPCOMINGDVDS tabLabel={{label: "UPCOMING DVDS"}} label="Page #9"/>
+                </ScrollableTabView>
+            
               </View>
             </DrawerLayoutAndroid>
         );
---
+++ src/component/TABS/IMDBTOP250.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text,View} from 'react-native';
+import {height,width,totalSize} from 'react-native-dimension';
+
+export default class IMDBTOP250 extends Component {
+    render () {
+        return(
+            <View>
+                <Text>Dummy</Text>
+                </View>
+        );
+
+        }
+}
